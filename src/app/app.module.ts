4c23e6189b0a9d09f572668b9e49db2f670753c9
Alexey Gubanich <Alexey_Gubanich@epam.com>
2019-04-08 11:57:09 +0300

    init login-input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { LoginInputComponent } from "./login-input/login-input.component";
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, LoginInputComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent]
